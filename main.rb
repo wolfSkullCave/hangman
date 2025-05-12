f51a8c57ff529fb58222bea947ea18d0202a2f05
Kyle-Laptop <67805222+wolfSkullCave@users.noreply.github.com>
2025-05-12 12:59:32 +0200

    Reworked hidden_word method and changed @word to an array instead of a string.

--- main.rb
+++ main.rb
@@ -4,6 +4,7 @@ class Hangman
 
   def initialize(filename)
     @wordslist = []
+    @hidden_word = []
     @filename = filename
   end
 
@@ -11,7 +12,6 @@ class Hangman
     f = File.open(@filename)
     while line = f.gets
       if line.length > 5 and line.length < 12
-        # puts line
         @wordslist << line
       end
     end
@@ -20,15 +20,23 @@ class Hangman
   end
 
   def shuffle_word
-    @word = @wordslist.shuffle.first
+    @word = @wordslist.shuffle.first.chars
   end
 
   def hidden_word
-    @word.each_char { |char| print '_ ' }
+    @word.each {|e| @hidden_word << '_'}
+    return @hidden_word
+  end
+
+  def guess_word(letter)
+
   end
 end
 
+guesses = ['a', 'e', 'i', 'o', 'u']
+
 play = Hangman.new('words.txt')
 play.read_file
-puts play.word
-play.hidden_word
+print play.word
+puts ''
+print play.hidden_word
