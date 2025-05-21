b1ef73f5bd3e5d6abf17e74ebb415c306d687c44
Kyle-Laptop <67805222+wolfSkullCave@users.noreply.github.com>
2025-05-21 12:27:47 +0200

    Refactor Hangman class and fix game logic
    - Moved Hangman class to its own file (hangman.rb)
    - Refactored class structure and code organization
    - Fixed bug where hidden_word displayed extra character, breaking win detection
    - Fixed bug where '/load' was added to guess array during load_game
    - Added and updated savegame files for game state persistence

---
+++ hangman.rb
@@ -0,0 +1,131 @@
+require 'json'
+require 'pry-byebug'
+
+class Hangman
+  attr_reader :word, :hidden_word, :attempts, :filename, :guess, :wordslist
+
+  def initialize(filename)
+    # declare variables
+    @filename = filename
+    @word = []
+    @wordslist = []
+    @hidden_word = []
+    @guess = []
+    @attempts = 12
+
+    # initialize variables
+    read_file
+    set_word
+    set_hidden_word
+  end
+
+  def start_game
+    # byebug
+    until game_over?
+      display_status
+      letter = get_guess
+
+      case letter
+      when '/save'
+        save_game
+        break
+      when '/load'
+        load_game
+      end
+
+      check_word_for(letter)
+      game_over?
+    end
+  end
+
+  private
+
+  def read_file
+    # Reads a list of words from a file and adds them to a wordlist array.
+    f = File.open(@filename)
+    while line = f.gets
+      line = line.strip # Remove newline and whitespace
+      @wordslist << line if line.length > 5 and line.length < 12
+    end
+  rescue StandardError => e
+    puts "Error reading file: #{e.message}"
+    @wordslist = []
+  end
+
+  def set_hidden_word
+    @hidden_word = Array.new(@word.length, '_')
+  end
+
+  def set_word
+    @word = @wordslist.sample.chars
+  end
+
+  def display_status
+    puts "word: #{@hidden_word.join(' ')}"
+    puts "guessed letters: #{@guess.uniq.join(' ')}"
+    puts "attempts left: #{@attempts}"
+    puts 'Save game with: /save'
+    puts 'Load game with /load'
+  end
+
+  def get_guess
+    puts 'Guess a letter: '
+    gets.chomp.downcase
+  end
+
+  def check_word_for(letter)
+    if @word.include?(letter)
+      @word.each_with_index do |item, index|
+        @hidden_word[index] = letter if item == letter.strip
+      end
+    elsif letter == '/load'
+      if @guess.include?('/load')
+        @guess.delete('/load')
+      end
+    else
+      @attempts -= 1
+      @guess << letter
+    end
+
+  end
+
+  def game_over?
+    if @attempts == 0
+      puts 'Game Over'
+      puts "Word: #{@word.join}"
+      true
+    elsif @word == @hidden_word
+      puts 'You Win'
+      true
+    end
+  end
+
+  def save_game(filename = 'game_save.json')
+    game_data = {
+      word: @word,
+      hidden_word: @hidden_word,
+      attemps: @attempts,
+      guesses: @guess
+    }
+
+    File.open(filename, 'w') do |file|
+      file.write(JSON.pretty_generate(game_data))
+    end
+
+    puts 'Game saved to game_save'
+  end
+
+  def load_game(filename = 'game_save.json')
+    if File.exist?(filename)
+      data = JSON.parse(File.read(filename), symbolize_names: true)
+      @word = data[:word]
+      @hidden_word = data[:hidden_word]
+      @attempts = data[:attemps]
+      @guess = data[:guesses]
+
+      puts 'Game loaded.'
+    else
+      puts 'No saved game found.'
+    end
+  end
+end
